3c9616a334b0603d1a0308d427ddda6b6e4dc6ae
ariful4082 <ariful4082@gmail.com>
2023-10-18 23:22:01 +0600

    html path with absolute and relative

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById('land').innerHTML = "Motherland";
+document.getElementById('land').style.color="red";
+document.getElementById('beauty').style.fontSize="25px"
+function changeimg(){
+document.getElementById('img').src="images/laptop.png"
+}
